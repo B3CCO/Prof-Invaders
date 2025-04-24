2de4d1f373f0360afffd7fc4588d3453cbce8d1b
Edoardo Pierucci <3doardopierucci@gmail.com>
2025-04-24 11:45:23 +0200

    aggiunto application

--- src/it/edu/iisgubbio/profinvaders/Prova.java
+++ src/it/edu/iisgubbio/profinvaders/Prova.java
@@ -1,5 +1,15 @@
 package it.edu.iisgubbio.profinvaders;
 
-public class Prova {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Prova extends Application{
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+	
 
 }
