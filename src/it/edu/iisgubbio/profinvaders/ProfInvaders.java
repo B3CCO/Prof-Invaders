7dfb641256eebd35cd151118fc08592895b0f4de
Nicola Becchetti <nicobecc08@gmail.com>
2025-04-29 10:56:05 +0200

    aggiunti invasori

---
+++ src/it/edu/iisgubbio/profinvaders/ProfInvaders.java
@@ -0,0 +1,119 @@
+package it.edu.iisgubbio.profinvaders;
+
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProfInvaders extends Application {
+    Pane sfondo = new Pane();
+    Rectangle eddyNico = new Rectangle(50, 20, Color.GREEN); 
+    List<Rectangle> colpi = new ArrayList<>();  
+    List<Rectangle> invasori = new ArrayList<>(); 
+    int sX = 225;
+ 
+    int velocitaColpi = 5;  
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        sfondo.setPrefSize(500, 500);
+        sfondo.getChildren().add(eddyNico);
+
+        eddyNico.setY(470);
+        eddyNico.setX(sX);
+        
+
+        Scene scena = new Scene(sfondo);
+        primaryStage.setScene(scena);
+        primaryStage.setTitle("Prof Invaders");
+        primaryStage.show();
+        
+   	 Rectangle invasore = new Rectangle(50, 30, Color.BLUE);
+   	 for(int a=0;a<10;a++) {
+   		 int i=a-1;
+   		 if(a==0) {
+   			 invasore.setX(50);
+   			 invasore.setY(50);
+   		 }else {
+   			 if(a-i==1) {
+   				 invasore.setX(invasore.getX()+20);
+   			 }
+   		 }
+   		 invasori.add(invasore);
+   		 sfondo.getChildren().add(invasore);
+   		 
+   	 }
+        
+        
+        Timeline muoviColpi = new Timeline(new KeyFrame(Duration.millis(20), e -> MuoviColpi()));
+        muoviColpi.setCycleCount(Timeline.INDEFINITE);
+        muoviColpi.play();
+
+        
+        scena.setOnKeyPressed(e -> TastoPremuto(e));
+    }
+
+    private void TastoPremuto(KeyEvent tasto) {
+        if (tasto.getCode() == KeyCode.RIGHT) { 
+            sX = sX + 7;
+            if (sX > 450) {
+                sX = 450;  
+            }
+        }
+        if (tasto.getCode() == KeyCode.LEFT) {
+            sX = sX - 7;
+            if (sX < 0) {
+                sX = 0;  
+            }
+        }
+        if (tasto.getCode() == KeyCode.SPACE) {
+  
+            Colpo();
+        }
+        eddyNico.setX(sX);
+    }
+
+    private void Colpo() {
+        
+        Rectangle colpo = new Rectangle(5, 10, Color.RED);
+        colpo.setX(eddyNico.getX() + 22); 
+        colpo.setY(eddyNico.getY());
+        
+        colpi.add(colpo);
+        sfondo.getChildren().add(colpo);
+
+
+    }
+
+    private void MuoviColpi() {
+       
+        List<Rectangle> toRemove = new ArrayList<>();
+        for (Rectangle colpo : colpi) {
+            colpo.setY(colpo.getY() - velocitaColpi); 
+
+            
+            if (colpo.getY() < 0) {
+                toRemove.add(colpo);
+            }
+        }
+
+        
+        colpi.removeAll(toRemove);
+        sfondo.getChildren().removeAll(toRemove);
+    }
+    
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}  
