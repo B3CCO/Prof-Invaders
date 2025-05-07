762b6e154b0453e73950ff4aa92ca622409b6d96
Nicola Becchetti <nicobecc08@gmail.com>
2025-05-07 09:50:06 +0200

    creato spostamento e altri invasori

--- src/it/edu/iisgubbio/profinvaders/ProfInvaders.java
+++ src/it/edu/iisgubbio/profinvaders/ProfInvaders.java
@@ -3,6 +3,7 @@ package it.edu.iisgubbio.profinvaders;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.application.Application;
+import javafx.geometry.Bounds;
 import javafx.scene.Scene;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
@@ -16,153 +17,186 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ProfInvaders extends Application {
-    Pane sfondo = new Pane();
-    Rectangle eddyNico = new Rectangle(50, 20, Color.GREEN); 
-    List<Rectangle> colpi = new ArrayList<>();  
-    List<Rectangle> invasori = new ArrayList<>(); 
+	Pane sfondo = new Pane();
+	Rectangle eddyNico = new Rectangle(50, 20, Color.GREEN); 
+	List<Rectangle> colpi = new ArrayList<>();  
+	List<Rectangle> invasori = new ArrayList<>(); 
 
-    int sX = 225;
- 
-    int velocitaColpi = 5;  
+	int sX = 225;
 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        sfondo.setPrefSize(500, 500);
-        sfondo.getChildren().add(eddyNico);
+	int velocitaColpi = 5;  
 
-        eddyNico.setY(470);
-        eddyNico.setX(sX);
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		sfondo.setPrefSize(500, 500);
+		sfondo.getChildren().add(eddyNico);
 
-        Scene scena = new Scene(sfondo);
-        primaryStage.setScene(scena);
-        primaryStage.setTitle("Prof Invaders");
-        primaryStage.show();
-        
+		eddyNico.setY(470);
+		eddyNico.setX(sX);
 
-        for(int cInvasore=0;cInvasore<15;cInvasore++) {
-        	int riga=cInvasore/5;
-        	int colonna=cInvasore%5;
-        	Rectangle invasore = new Rectangle(50, 30, Color.BLUE);
-        	
-        	invasore.setX(70+colonna*75);
-        	invasore.setY(40+riga*40);
-
-        	invasori.add(invasore);
-        	sfondo.getChildren().add(invasore);
-
-        }
-        
-        Timeline muoviInvasoriDx = new Timeline(new KeyFrame(Duration.seconds(0.7), e -> MuoviInvasoriDx()));
-        muoviInvasoriDx.setCycleCount(Timeline.INDEFINITE);
-        muoviInvasoriDx.play();
-        
-        Timeline muoviInvasoriSx = new Timeline(new KeyFrame(Duration.seconds(0.7), e -> MuoviInvasoriSx()));
-        muoviInvasoriSx.setCycleCount(Timeline.INDEFINITE);
-        muoviInvasoriSx.play();
-        
-        
-        Timeline muoviInvasoriY = new Timeline(new KeyFrame(Duration.seconds(2), e -> MuoviInvasoriY()));
-        muoviInvasoriY.setCycleCount(Timeline.INDEFINITE);
-        muoviInvasoriY.play();
+		Scene scena = new Scene(sfondo);
+		primaryStage.setScene(scena);
+		primaryStage.setTitle("Prof Invaders");
+		primaryStage.show();
 
 
-   	    Timeline muoviColpi = new Timeline(new KeyFrame(Duration.millis(20), e -> MuoviColpi()));
-        muoviColpi.setCycleCount(Timeline.INDEFINITE);
-        muoviColpi.play();
+		for(int cInvasore=0;cInvasore<10;cInvasore++) {
+			int riga=cInvasore/5;
+			int colonna=cInvasore%5;
+			Rectangle invasore = new Rectangle(50, 30, Color.BLUE);
 
-        scena.setOnKeyPressed(e -> TastoPremuto(e));
-    }
+			invasore.setX(70+colonna*75);
+			invasore.setY(40+riga*40);
 
-    private void TastoPremuto(KeyEvent tasto) {
-        if (tasto.getCode() == KeyCode.RIGHT) { 
-            sX = sX + 7;
-            if (sX > 450) {
-                sX = 450;  
-            }
-        }
-        if (tasto.getCode() == KeyCode.LEFT) {
-            sX = sX - 7;
-            if (sX < 0) {
-                sX = 0;  
-            }
-        }
-        if (tasto.getCode() == KeyCode.SPACE) {
-  
-            Colpo();
-        }
-        eddyNico.setX(sX);
-    }
+			invasori.add(invasore);
+			sfondo.getChildren().add(invasore);
 
-    private void Colpo() {
-    	
-    	Rectangle colpo = new Rectangle(5, 10, Color.RED);
-        colpo.setX(eddyNico.getX() + 22); 
-        colpo.setY(eddyNico.getY());
-        
-        colpi.add(colpo);
-        sfondo.getChildren().add(colpo);
+		}
 
+		Timeline muoviInvasoriDx = new Timeline(new KeyFrame(Duration.millis(18), e -> MuoviInvasori()));
+		muoviInvasoriDx.setCycleCount(Timeline.INDEFINITE);
+		muoviInvasoriDx.play();
+				
+		Timeline muoviColpi = new Timeline(new KeyFrame(Duration.millis(20), e -> MuoviColpi()));
+		muoviColpi.setCycleCount(Timeline.INDEFINITE);
+		muoviColpi.play();
 
-    }
+		scena.setOnKeyPressed(e -> TastoPremuto(e));
+		
+	}
 
-    private void MuoviColpi() {
-       
-        List<Rectangle> fuori = new ArrayList<>();
-        for (Rectangle colpo : colpi) {
-            colpo.setY(colpo.getY() - velocitaColpi); 
+	private void TastoPremuto(KeyEvent tasto) {
+		
+		if (tasto.getCode() == KeyCode.RIGHT) { 
+						sX = sX + 7;
+			if (sX > 450) {
+				sX = 450;  
+			}
+		}
+		if (tasto.getCode() == KeyCode.LEFT) {
+						sX = sX - 7;
+			if (sX < 0) {
+				sX = 0;  
+			}
+		}
+		if (tasto.getCode() == KeyCode.SPACE) {
 
-            
-            if (colpo.getY() < 0) {
-                fuori.add(colpo);
-            }
-        }
+			Colpo();
+		}
+		eddyNico.setX(sX);
+	}
 
-        
-        colpi.removeAll(fuori);
-        sfondo.getChildren().removeAll(fuori);
+	
+	private void Colpo() {
 
-    }
-    
-    private void MuoviInvasoriY() {
-    	List<Rectangle> eliminato = new ArrayList<>();
-    	
-    	for(Rectangle invasore:invasori) {
-    		invasore.setY(invasore.getY()+5);
-    		
-            if (invasore.getY() > 480) {
-                eliminato.add(invasore);
-            }
-    	}
+		Rectangle colpo = new Rectangle(5, 10, Color.RED);
+		colpo.setX(eddyNico.getX() + 22); 
+		colpo.setY(eddyNico.getY());
 
-    	invasori.removeAll(eliminato);
-        sfondo.getChildren().removeAll(eliminato);
-    	
-    }
-    
-    private void MuoviInvasoriDx() {
-    	
-    	for(Rectangle invasore:invasori) {
-    		invasore.setX(invasore.getX()+5);
-    		invasore.setX(invasore.getX()+5);
-    	}
-    }
+		colpi.add(colpo);
+		sfondo.getChildren().add(colpo);
 
-    private void MuoviInvasoriSx() {
+	}
 
-    	for(Rectangle invasore:invasori) {
-    		invasore.setX(invasore.getX()-15);
-    		
-    	}
-    }
+	private void MuoviColpi() {
 
-    /* private void Collisione() {
-    	Bounds 
-    }*/
+		List<Rectangle> fuori = new ArrayList<>();
+				
+		for (int c=0; c< colpi.size();c++) {
+			Rectangle colpo=colpi.get(c);
+			colpo.setY(colpo.getY() - velocitaColpi); 
 
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+			if (colpo.getY() < 0) {
+				fuori.add(colpo);
+			}
+
+			for(int j=0;j<colpi.size();j++) {
+
+				Bounds b= colpi.get(j).getBoundsInParent();
+
+				for(int i=0;i<invasori.size();i++) {
+					Rectangle invasore=invasori.get(i);
+					Bounds b1= invasore.getBoundsInParent();
+
+					if(b.intersects(b1)) {
+						invasori.remove(i);
+						colpi.remove(j);
+						sfondo.getChildren().remove(invasore);
+						sfondo.getChildren().remove(colpo);
+					}
+				}
+			}         
+		}
+		
+		colpi.removeAll(fuori);
+		sfondo.getChildren().removeAll(fuori);
+	}
+
+	
+	boolean muoviVersoDestra = true;
+	private void MuoviInvasori() {
+
+		for(Rectangle invasore:invasori) {
+			
+			if(muoviVersoDestra){
+				invasore.setX(invasore.getX()+5);
+			}else {
+				invasore.setX(invasore.getX()-5);
+				
+			}
+			
+			
+		}
+		if(invasori.get(invasori.size()-1).getX()>=440) {
+			muoviVersoDestra=false;
+			for(Rectangle inv:invasori){
+				inv.setY(inv.getY()+3);
+			}
+			
+		}
+		if(invasori.get(0).getX() <= 10) {
+			muoviVersoDestra = true;
+			for(int a=0; a<invasori.size();a++){
+				Rectangle inv=invasori.get(a);
+				inv.setY(inv.getY()+3);
+			}
+		}
+		
+		if(invasoriInFondo()) {
+			resetInvasori();
+		}
+	}
+	
+	private boolean invasoriInFondo() {
+		for (Rectangle invasore : invasori) {
+	        if (invasore.getY() <= 500) {
+	            return false;
+	        }
+	    }
+		return true;
+	}
+	
+	private void resetInvasori() {
+		invasori.clear();
+		
+		for(int cInvasore=0;cInvasore<10;cInvasore++) {
+			int riga=cInvasore/5;
+			int colonna=cInvasore%5;
+			Rectangle invasore = new Rectangle(50, 30, Color.BLUE);
+
+			invasore.setX(70+colonna*75);
+			invasore.setY(40+riga*40);
+
+			invasori.add(invasore);
+			sfondo.getChildren().add(invasore);
+
+		}
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
 
 }  
 
